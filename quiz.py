cc01970ac233c40b306dc047c7b52cb2d9ef166c
Andrew Park <atpark333@gmail.com>
2022-06-15 00:37:04 +0900

    Update program to use new data format

--- quiz.py
+++ quiz.py
@@ -1,33 +1,47 @@
 import random
 from string import ascii_lowercase
 
+import pathlib
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
 
 NUM_QUESTIONS_PER_QUIZ = 5
-QUESTIONS = {
-    "When was the first known use of the word 'quiz'": [
-        "1781", "1771", "1871", "1881"
-    ],
-    "Which built-in function can get information from the user": [
-        "input", "get", "print", "write"
-    ],
-    "What is the capital of Portugal": [
-        "Lisbon", "Paris", "Rio", "Madrid"
-    ],
-    "Which company created JavaScript": [
-        "Netscape", "Mozilla", "Google", "Microsoft"
-    ],
-    "What's the official name of the := operator": [
-        "Assignment expression",
-        "Named expression",
-        "Walrus operator",
-        "Colon equals operator"
-    ]
-}
+QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
 
 
-def prepare_questions(questions: list, num_questions: int):
+def run_quiz():
+    questions = prepare_questions(
+        QUESTIONS_PATH, num_questions=NUM_QUESTIONS_PER_QUIZ
+    )
+
+    num_correct = 0
+    for num, question in enumerate(questions, start=1):
+        print(f"\nQuestion {num}:")
+        num_correct += ask_question(question)
+
+    print(f"\n You got {num_correct} correct out of {num} questions")
+
+
+def prepare_questions(path, num_questions: int):
+    questions = tomllib.loads(path.read_text())["questions"]
     num_questions = min(num_questions, len(questions))
-    return random.sample(list(questions.items()), k=num_questions)
+    return random.sample(questions, k=num_questions)
+
+
+def ask_question(question):
+    correct_answer = question["answer"]
+    alternatives = [question["answer"]] + question["alternatives"]
+    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
+
+    answer = get_answer(question["question"], ordered_alternatives)
+    if answer == correct_answer:
+        print("⭐ Correct! ⭐")
+        return 1
+    else:
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
+        return 0
 
 
 def get_answer(question: str, alternatives: list):
@@ -40,32 +54,6 @@ def get_answer(question: str, alternatives: list):
         print(f"Please enter one of {', '.join(labeled_alternatives)}")
 
     return labeled_alternatives[answer_label]
-
-
-def ask_question(question, alternatives):
-    correct_answer = alternatives[0]
-    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
-
-    answer = get_answer(question, ordered_alternatives)
-    if answer == correct_answer:
-        print("⭐ Correct! ⭐")
-        return 1
-    else:
-        print(f"The answer is {correct_answer!r}, not {answer!r}")
-        return 0
-
-
-def run_quiz():
-    questions = prepare_questions(
-        QUESTIONS, num_questions=NUM_QUESTIONS_PER_QUIZ
-    )
-
-    num_correct = 0
-    for num, (question, alternatives) in enumerate(questions, start=1):
-        print(f"\nQuestion {num}:")
-        num_correct += ask_question(question, alternatives)
-
-    print(f"\n You got {num_correct} correct out of {num} questions")
 
 
 if __name__ == "__main__":
